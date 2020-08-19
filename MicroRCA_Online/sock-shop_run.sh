f859af436ab8a36f27a7b44f55c1216bb6605503
TheDeng <dengjiale1996@163.com>
2020-08-19 10:48:56 +0800

    sock-shop dada enrich

---
+++ MicroRCA_Online/sock-shop_run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+
+for MS in 'shipping' 'payment' 'orders' 'user' 'carts' 'catalogue'
+do
+  kubectl apply -f /root/zik/microservices-demo/zik-test/sock-shop/$MS-delay.yaml
+
+  n=0
+  while (($n<10))
+  do
+    python3 tRCA_online.py --fault $MS &
+    python3 MicroRCA_online.py --fault $MS &
+    n=$((n+1))
+    sleep 10
+  done 
+
+  kubectl delete -f /root/zik/microservices-demo/zik-test/sock-shop/$MS-delay.yaml
+
+  sleep 150
+	wait
+done
+
+
