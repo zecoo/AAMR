a832a8a7f12dea8c97b99a81ef4b73acf81e9506
TheDeng <dengjiale1996@163.com>
2020-08-19 17:00:06 +0800

    failed to run locust online

--- locustfile.py
+++ locustfile.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 from random import randint, choice
 import base64
 from locust import HttpUser, task, between
--- run_locust.sh
+++ run_locust.sh
@@ -1,1 +1,1 @@
-locust --host="http://39.100.0.61:30001" -u 100 -r 20
+locust -f locustfile.py --host="http://39.100.0.61:30001" -u 100 -r 20
