59a0661d848321b3df5474e0026cad7297cd4c35
22Kon <2782596816@qq.com>
2020-08-26 16:24:26 +0800

    csv hisroty 32m delete

--- Online/headless_locust.sh
+++ Online/headless_locust.sh
@@ -1,1 +1,1 @@
-locust --host="http://39.100.0.61:30001" -u 100 -r 20 --headless --csv=example 1>/dev/null 2>/dev/null 
+locust --host="http://39.100.0.61:30001" -u 100 -r 20 --headless --run-time 30m --csv=example 1>/dev/null 2>/dev/null 
