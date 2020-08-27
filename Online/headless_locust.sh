fa2aa9bd2058032b0a8e6228c9662b8e9f6f52c9
TheDeng <dengjiale1996@163.com>
2020-08-27 18:55:16 +0800

    mscope data

--- Online/headless_locust.sh
+++ Online/headless_locust.sh
@@ -1,1 +1,1 @@
-locust --host="http://39.100.0.61:30001" -u 50 -r 20 --headless --run-time 10m --csv=example 1>/dev/null 2>/dev/null 
+locust --host="http://39.100.0.61:30001" -u 50 -r 20 --headless --run-time 25m --csv=example 1>/dev/null 2>/dev/null 
