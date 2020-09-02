7facbb1d6a25604dd1c6c26f96c1f7f6603881a7
TheDeng <dengjiale1996@163.com>
2020-09-02 20:49:44 +0800

    hipster data failed

---
+++ Hipster/test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+var='Hipster_online.py'
+
+./headless_locust.sh &
+
+echo '----- Locust started -----'
+echo '----- RCA will be started in 150s ... ----'
+
+function countdown() {
+  for i in $(seq 150 -5 5)
+  do
+    echo -e "${i}s left"
+    sleep 5
+    wait
+  done
+}
+
+for MS in 'adservice' 'shippingservice'
+do
+  countdown
+
+  kubectl apply -f /root/zik/fault-injection/hipster/$MS.yaml
+
+  n=0
+  while (($n<5))
+  do
+    python3 $var --fault $MS &
+    n=$((n+1))
+    sleep 10
+  done
+  
+  kubectl delete -f /root/zik/fault-injection/hipster/$MS.yaml
+
+done
+
+wait
+echo '---- Test ends ----'
