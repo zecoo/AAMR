05428a557570f2144448c66513f6162fbbc57136
22Kon <2782596816@qq.com>
2020-09-08 19:41:20 +0800

    try 2f hipster

---
+++ Hipster/main.py
@@ -0,0 +1,50 @@
+import os
+import time
+from itertools import combinations
+
+rca_arr = ['Microscope_online.py']
+svc_arr = ['cartservice', 'productcatalogservice']
+
+# rca_arr = ['Microscope_online.py', 'MicroRCA_online.py', 'tRCA_online.py']
+# svc_arr = ['cartservice', 'productcatalogservice', 'currencyservice', 'checkoutservice', 'recommendationservice', 'paymentservice',]
+
+def combine_svc():
+  comb_svc = list(combinations(svc_arr, 2))
+  svc_list = []
+  for svc in comb_svc:
+    svc = svc[0] + '+' + svc[1]
+    svc_list.append(svc)
+  return svc_list
+
+def countdown():
+  time_left = 180
+  while time_left > 0:
+    print('left: %s s' % time_left)
+    time.sleep(5)
+    time_left = time_left - 5
+
+if __name__ == '__main__':
+
+  os.system('./headless_locust.sh &')
+  print('----- Locust started -----')
+  print('----- RCA will be started in 3min ... ----')
+
+  svc_list = combine_svc()
+  for svcs in svc_list:
+    countdown()
+
+    svc2 = svcs.split('+')
+    for svc in svc2:
+      os.system('kubectl apply -f /root/zik/fault-injection/sock-shop/%s.yaml' % svc)
+
+    n = 0
+    while (n<10):
+      for rca in rca_arr:
+        os.system('python3 %s --fault %s &' % (rca, svcs))
+      time.sleep(6)
+      n = n + 1
+    
+    for svc in svc2:
+      os.system('kubectl delete -f /root/zik/fault-injection/sock-shop/%s.yaml' % svc)
+    
+  print('---- Test ends ----')
