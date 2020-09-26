4849267aefede497c7f4e9f7ba459aeee71d3f2c
22Kon <2782596816@qq.com>
2020-09-26 15:04:03 +0800

    scale deployment

---
+++ Hipster/scale_hipster.py
@@ -0,0 +1,22 @@
+import os
+
+svc_arr = ['paymentservice', 'currencyservice', 'cartservice', 'productcatalogservice', 'checkoutservice', 'recommendationservice']
+
+def parse_args():
+    """Parse the args."""
+    parser = argparse.ArgumentParser(
+        description='Root cause analysis for microservices')
+
+    parser.add_argument('--num', type=str, required=False,
+                        default='2',
+                        help='replica num')
+
+    return parser.parse_args()
+
+if __name__ == "__main__":
+
+    args = parse_args()
+    replica_num = args.num
+
+    for svc in svc_arr:
+        os.system('kubectl scale deployment -n hipster %s --replicas=%d' %(svc, replica_num))
