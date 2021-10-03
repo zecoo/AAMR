a30503e6d87fa1f648acdf82f1bae2fc87a8b6be
22Kon <2782596816@qq.com>
2021-10-03 11:21:31 +0800

    add source

--- Sock/headless_locust.sh
+++
@@ -1,5 +0,0 @@
-echo '----- Locust started -----'
-
-locust --host="http://39.100.0.61:30001" -u 2 -r 2 --headless --run-time 60m --csv=example 1>/dev/null 2>/dev/null & 
-
-exit
--- THipster/headless_locust.sh
+++
@@ -1,5 +0,0 @@
-echo '----- Locust started -----'
-
-locust --host="http://39.100.0.61:32580" -u 20 -r 5 --headless --run-time 60m --csv=example 1>/dev/null 2>/dev/null & 
-
-exit
